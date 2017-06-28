21e9508bf361f705a791f4e0397be2dfb1dbec9c
ali sherafat <sherafat01@gmail.com>
2017-06-28 22:32:40 +0430

    first project improved

--- p1.py
+++ p1.py
@@ -1,32 +1,5 @@
 from src.stack import Stack
-
-
-def infix2postfix(infix):
-    precedence = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
-    stack = Stack()
-    output = []
-
-    for token in infix:
-        # check for operand
-        if token in "abcdefghijklmnopqrstuvwxyz":
-            output.append(token)
-        elif token == '(':
-            stack.push(token)
-        elif token == ')':
-            top_token = stack.pop()
-            while top_token != '(':
-                output.append(top_token)
-                top_token = stack.pop()
-        else:
-            while (not stack.is_empty()) and \
-                    (precedence[stack.peek()] >= precedence[token]):
-                output.append(stack.pop())
-            stack.push(token)
-
-    while not stack.is_empty():
-        output.append(stack.pop())
-
-    return " ".join(output)
+from src.infix import Infix
 
 
 def postfix2infix(postfix):
@@ -44,37 +17,6 @@ def postfix2infix(postfix):
     return stack.pop()
 
 
-def infix2prefix(infix):
-    precedence = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
-    stack = Stack()
-
-    output = []
-    temp = []
-    for token in infix:
-        if token in "abcdefghijklmnopqrstuvwxyz":
-            output.append(token)
-        elif token == '(':
-            stack.push(token)
-        elif token == ')':
-            top_of_stack = stack.pop()
-            while top_of_stack != '(':
-                temp.append(top_of_stack)
-                top_of_stack = stack.pop()
-            output = temp + output
-            temp = []
-        else:
-            while (not stack.is_empty()) and \
-                    (precedence[stack.peek()] >= precedence[token]):
-                temp.append(stack.pop())
-            output = temp + output
-            temp = []
-            stack.push(token)
-    while not stack.is_empty():
-        temp.append(stack.pop())
-    output = temp + output
-    return ' '.join(output)
-
-
 def prefix2infix(prefix):
     reverse = prefix[::-1]
     temp = postfix2infix(reverse)
@@ -82,7 +24,7 @@ def prefix2infix(prefix):
     return infix
 
 
-print(infix2postfix("(a+b)*c-(d-e)*(f+g)"))
-print(infix2prefix("(a+b)*c-(d-e)*(f+g)"))
+print(Infix("(a+b)*c-(d-e)*(f+g)").get_postfix())
+print(Infix("(a+b)*c-(d-e)*(f+g)").get_prefix())
 print(postfix2infix("ab+c*de-fg+*-"))
 print(prefix2infix("+*+abd/e+f*ad"))
---
+++ src/infix.py
@@ -0,0 +1,63 @@
+from .stack import Stack
+
+
+class Infix:
+    def __init__(self, infix):
+        self.infix = infix
+
+    def get_postfix(self):
+        precedence = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
+        stack = Stack()
+        output = []
+
+        for token in self.infix:
+            # check for operand
+            if token in "abcdefghijklmnopqrstuvwxyz" or token in "0123456789":
+                output.append(token)
+            elif token == '(':
+                stack.push(token)
+            elif token == ')':
+                top_token = stack.pop()
+                while top_token != '(':
+                    output.append(top_token)
+                    top_token = stack.pop()
+            else:
+                while (not stack.is_empty()) and \
+                        (precedence[stack.peek()] >= precedence[token]):
+                    output.append(stack.pop())
+                stack.push(token)
+
+        while not stack.is_empty():
+            output.append(stack.pop())
+
+        return "".join(output)
+
+    def get_prefix(self):
+        precedence = {"*": 3, "/": 3, "+": 2, "-": 2, "(": 1}
+        stack = Stack()
+
+        output = []
+        temp = []
+        for token in self.infix:
+            if token in "abcdefghijklmnopqrstuvwxyz":
+                output.append(token)
+            elif token == '(':
+                stack.push(token)
+            elif token == ')':
+                top_of_stack = stack.pop()
+                while top_of_stack != '(':
+                    temp.append(top_of_stack)
+                    top_of_stack = stack.pop()
+                output = temp + output
+                temp = []
+            else:
+                while (not stack.is_empty()) and \
+                        (precedence[stack.peek()] >= precedence[token]):
+                    temp.append(stack.pop())
+                output = temp + output
+                temp = []
+                stack.push(token)
+        while not stack.is_empty():
+            temp.append(stack.pop())
+        output = temp + output
+        return ' '.join(output)
